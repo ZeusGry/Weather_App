de6955a672f10b3507bdeb8c3a81b5a1651be99b
Mateusz Kapuscinski <wojtkom77@o2.pl>
2021-04-23 18:45:31 +0200

    utworzenie klasy abstrakcyjnej i jednej encji

---
+++ App/src/main/java/Weather/OpenWeatherMap.java
@@ -0,0 +1,20 @@
+package Weather;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OpenWeatherMap extends WeatherHelper{
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+
+    public OpenWeatherMap(Double temperature, Double pressure, Double humidity, Double speedWind, Double directionWind) {
+        super(temperature, pressure, humidity, speedWind, directionWind);
+    }
+
+    public OpenWeatherMap() {
+    }
+}
